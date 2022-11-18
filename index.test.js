2fecf8dfaa3733c56dbe5ba54519d62ac22469ab
demon166 <demon166@mail.ru>
2022-11-18 21:48:03 +0300

    init

---
+++ index.test.js
@@ -0,0 +1,6 @@
+const { stringToArray, deleteCharacters, insertDash, func1, capitalize, changeRegister, removeChar,
+    zeros, comparison, insensitiveSearch, initCap, initSnake, repeatStr, path, endsWith, getSubstr,
+    insert, limitStr, count, strip, cutString, findWord
+} = require('.');
+
+
